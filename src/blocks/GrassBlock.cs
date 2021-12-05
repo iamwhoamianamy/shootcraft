76689f711a3514fcdf5fd8a81d0e554513e09f7a
zadolbali10 <anon.yu@mail.ru>
2021-12-05 16:21:01 +0700

    Implemented decaying of grass

--- src/blocks/GrassBlock.cs
+++ src/blocks/GrassBlock.cs
@@ -12,5 +12,13 @@ namespace shootcraft.src.blocks
    {
       public GrassBlock() : base() { }
       public GrassBlock(Vector2 pos) : base(pos) { }
+
+      public override void Update()
+      {
+         Block block = World.GetBlock(pos, 0, 1);
+
+         if (!(block is null) && !(block is AirBlock))
+            World.SetBlock(new DirtBlock(pos));
+      }
    }
 }
